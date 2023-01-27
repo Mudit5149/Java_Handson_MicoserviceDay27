32ccfbec12070c2f41cff8a84857c8a47b704fa3
Mudit5149 <86524837+Mudit5149@users.noreply.github.com>
2023-01-27 18:08:20 +0530

    Add files via upload

---
+++ ObjectPrefrenceDemo.java
@@ -0,0 +1,13 @@
+
+public class ObjectPrefrenceDemo {
+	int x;
+	ObjectPrefrenceDemo(){
+		
+	}
+	public static void main(String[] args) {
+		 ObjectPrefrenceDemo ord =new  ObjectPrefrenceDemo();
+		 System.out.println(ord.x);
+
+	}
+
+}
